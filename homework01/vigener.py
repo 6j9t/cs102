4b074260969102a176b9669136679e56bf91f2c9
Ruzel <ruzel.yusupov@gmail.com>
2019-10-13 13:09:49 +0300

    Реализованы функции encrypt_vigenere и decrypt_vigenere

--- homework01/vigener.py
+++ homework01/vigener.py
@@ -8,6 +8,22 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
     'LXFOPVEFRNHR'
     """
     # PUT YOUR CODE HERE
+    ciphertext = ''
+    shift = 0
+    for ch in plaintext:
+        if 'a' <= ch <= 'z' or 'A' <= ch <= 'Z':
+            if ('a' <= ch <= 'z'):
+                newch = ord(ch) + ord(keyword[shift % len(keyword)]) - ord('a')
+                if (chr(newch) > 'z'):
+                    newch -= 26
+            else:
+                newch = ord(ch) + ord(keyword[shift % len(keyword)]) - ord('A')
+                if (chr(newch) > 'Z'):
+                    newch -= 26
+            ciphertext += chr(newch)
+        else:
+            ciphertext += ch
+        shift += 1
     return ciphertext
 
 
@@ -21,4 +37,20 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     # PUT YOUR CODE HERE
+    plaintext = ''
+    shift = 0
+    for ch in ciphertext:
+        if ('a' <= ch <= 'z' or 'A' <= ch <= 'Z'):
+            if ('a' <= ch <= 'z'):
+                newch = ord(ch) - ord(keyword[shift % len(keyword)]) + ord('a')
+                if (chr(newch) < 'a'):
+                    newch += 26
+            else:
+                newch = ord(ch) - ord(keyword[shift % len(keyword)]) + ord('A')
+                if (chr(newch) < 'A'):
+                    newch += 26
+            plaintext += chr(newch)
+        else:
+            plaintext += ch
+        shift += 1
     return plaintext
