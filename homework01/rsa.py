35f445be269906ac183cdff88a93d335b58b4b68
Ruzel <ruzel.yusupov@gmail.com>
2019-10-13 15:24:38 +0300

    Реализована функция поиска НОД

--- homework01/rsa.py
+++ homework01/rsa.py
@@ -31,7 +31,9 @@ def gcd(a, b):
     1
     """
     # PUT YOUR CODE HERE
-    pass
+    while (b!=0):
+        a, b = b, a%b
+    return a
 
 
 def multiplicative_inverse(e, phi):
@@ -54,9 +56,11 @@ def generate_keypair(p, q):
 
     # n = pq
     # PUT YOUR CODE HERE
+    n = p * q
 
     # phi = (p-1)(q-1)
     # PUT YOUR CODE HERE
+    phi = (p-1) * (q - 1)
 
     # Choose an integer e such that e and phi(n) are coprime
     e = random.randrange(1, phi)
