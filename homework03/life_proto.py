05d4b4e5a072e960c1cc3ea956d3ae4cd672a49d
Ruzel <ruzel.yusupov@gmail.com>
2019-11-12 19:58:04 +0300

    Реализована функция create_grid()

--- homework03/life_proto.py
+++ homework03/life_proto.py
@@ -4,7 +4,6 @@ import random
 from pygame.locals import *
 from typing import List, Tuple
 
-
 Cell = Tuple[int, int]
 Cells = List[int]
 Grid = List[Cells]
@@ -12,7 +11,7 @@ Grid = List[Cells]
 
 class GameOfLife:
 
-    def __init__(self, width: int=640, height: int=480, cell_size: int=10, speed: int=10) -> None:
+    def __init__(self, width: int = 640, height: int = 480, cell_size: int = 10, speed: int = 10) -> None:
         self.width = width
         self.height = height
         self.cell_size = cell_size
@@ -33,10 +32,10 @@ class GameOfLife:
         """ Отрисовать сетку """
         for x in range(0, self.width, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color('black'),
-                    (x, 0), (x, self.height))
+                             (x, 0), (x, self.height))
         for y in range(0, self.height, self.cell_size):
             pygame.draw.line(self.screen, pygame.Color('black'),
-                    (0, y), (self.width, y))
+                             (0, y), (self.width, y))
 
     def run(self) -> None:
         """ Запустить игру """
@@ -63,7 +62,7 @@ class GameOfLife:
             clock.tick(self.speed)
         pygame.quit()
 
-    def create_grid(self, randomize: bool=False) -> Grid:
+    def create_grid(self, randomize: bool = False) -> Grid:
         """
         Создание списка клеток.
 
@@ -81,7 +80,12 @@ class GameOfLife:
         out : Grid
             Матрица клеток размером `cell_height` х `cell_width`.
         """
-        pass
+        list = [[0 for i in range(self.cell_width)] for i in range(self.cell_height)]
+        if randomize == 1:
+            for i in range(self.cell_height):
+                for j in range(self.cell_width):
+                    list[i][j] = random.randrange(0, 2)
+        return list
 
     def draw_grid(self) -> None:
         """
